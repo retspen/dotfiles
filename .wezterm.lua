068762c4d4f319f70bd038468699f03ce58bfeb1
Anatoliy Guskov <anatoliy.guskov@gmail.com>
2023-01-10 23:34:26 +0200

    Create .wezterm.lua

---
+++ .wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require 'wezterm'
+
+return {
+ enable_tab_bar = false,
+ hide_tab_bar_if_only_one_tab = true,
+ color_scheme = 'Builtin Solarized Dark',
+ font = wezterm.font 'JetBrains Mono',
+ font_size = 15.0
+}
