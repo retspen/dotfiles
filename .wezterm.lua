e91fda62b6d73d29bbdd89e2f378267c437212cd
Anatoliy Guskov <anatoliy.guskov@gmail.com>
2024-04-05 22:28:31 +0300

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,9 +4,9 @@ return {
  enable_tab_bar = false,
  hide_tab_bar_if_only_one_tab = true,
  window_close_confirmation = 'NeverPrompt',
- color_scheme = 'Builtin Solarized Dark', 
+ color_scheme = 'Dracula', 
  font = wezterm.font 'JetBrains Mono',
- font_size = 15.0,
+ font_size = 11.0,
  window_padding = {
   left = 10,
   right = 10,
